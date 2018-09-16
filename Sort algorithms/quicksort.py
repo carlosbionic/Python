ba9bcb809b39699ce56da2962c6e8cd9be5e645a
Carlos Prado <carlosaprado@gmail.com>
2018-09-16 14:27:30 -0300

    Update quicksort.py

--- Sort algorithms/quicksort.py
+++ Sort algorithms/quicksort.py
@@ -1,42 +1,29 @@
-#Metodo ordenamiento
-#A partir de un array no ordenado
-#Elijo un elemento al azar (pivot)
-#Ordeno los elementos mayores al pivot y los pongo arriba
-#Ordeno los elementos menores al pivot y los pongo abajo
+# QuickSort
 
-import random as rn
+def quickSort(arr):
 
-arr=[1,1,2,5,4,5,4,2,1,1,7,8,5,7,4,31,-1,2,13,0,5] #Array a ordenar
+    great = []
+    less = []
+    equal = []
 
-def quicksort(array):
+    if len(arr) > 1:
 
-    mayor=[]
-    menor=[]
-    igual=[]
+        pivot = arr[-1] # Take last element of array as pivot
 
-    if len(array) > 1: 
-      
+        for elem in arr:
 
-        ran = rn.choice(array) #Elijo un elemento al azar
-	pivot = ran
-        #pivot = array[0]
-        #print pivot
-        #print len(array)
+            if elem > pivot:
+                great += [elem]
+            elif elem < pivot:
+                less += [elem]
+            elif elem == pivot:
+                equal += [elem]
 
-        
-
-        for i in array:
-            if i > pivot:
-                mayor=mayor+[i]
-            elif i < pivot:
-                menor=menor+[i]
-            elif i == pivot:
-                igual=igual+[i]
-
-        return quicksort(mayor)+igual+quicksort(menor)
+        return quickSort(less) + equal + quickSort(great)
 
     else:
-        return array
-        
-    
-print quicksort(arr)
+
+        return arr
+
+arr = [1, 5, 3, 11, 2, 3, 8]
+print(quickSort(arr))
